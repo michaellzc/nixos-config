717612457dd37ace902bb307d976be4daabb12b8
Michael Lin <mlzc@hey.com>
2022-01-20 11:43:34 -0800

    Change user password

--- users/michael/nixos.nix
+++ users/michael/nixos.nix
@@ -9,7 +9,7 @@
     home = "/home/michael";
     extraGroups = [ "docker" "wheel" ];
     shell = pkgs.fish;
-    hashedPassword = "$6$p5nPhz3G6k$6yCK0m3Oglcj4ZkUXwbjrG403LBZkfNwlhgrQAqOospGJXJZ27dI84CbIYBNsTgsoH650C1EBsbCKesSVPSpB1";
+    hashedPassword = "$6$6P6Q6RKyUN5CjfV7$JeLLcrP/Da2mJYEmWwKmBwtvn.Vsem4kVZ3yooT8ChO23d9ei0yqXQprnBlyF3mvFLi3h6fGqWtfsj/5vh/FS/";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEAz0NIBAuVDcTAZMrgLXd6P6Kfuk8t6+CeFBkvAIlLr hi@michaellin.me"
     ];
